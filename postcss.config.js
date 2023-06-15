15578aa6517914e5063fc3f3c7695cc501fe4bdd
Seth Warburton <seth@madebykind.com>
2023-06-15 13:47:00 +0100

    [Improved] Remove unused libs

--- postcss.config.js
+++ postcss.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   plugins: [
     require('postcss-import'),
-    require('tailwindcss/nesting')(require('postcss-nesting')),
+    require('tailwindcss/nesting'),
     require('tailwindcss'),
     require('postcss-preset-env')({
       features: { 'nesting-rules': false }
