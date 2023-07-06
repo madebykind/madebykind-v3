d3e048665a5a894355b8d9b0c491ad79ba07b8d4
Mat Hayward <mat@madebykind.com>
2023-07-06 11:35:35 +0100

    JS to set height of homepage heroes

--- src/js/modulesEnabled.js
+++ src/js/modulesEnabled.js
@@ -1,9 +1,11 @@
 
 import { bindFilters } from "@/js/topic-filters";
 import { bindToggles } from "@/js/toggle";
+import { bindHeroHeight } from "@/js/hero-height";
 
 bindFilters()
 bindToggles()
+bindHeroHeight()
 
 export default {
 
