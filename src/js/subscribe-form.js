bd33061d5219a4b216189bee6ced5658ea0467f6
Mat Hayward <mat@madebykind.com>
2024-02-07 14:28:05 +0000

    Honeypot field and gifted campaign form

--- src/js/subscribe-form.js
+++ src/js/subscribe-form.js
@@ -2,23 +2,29 @@ function SubscribeForm(el) {
   const action = el.getAttribute('action');
   const form = el.querySelector("[data-form]");
   const message = el.querySelector("[data-message]");
+  const honeypotField = el.querySelector("input.h0n")
 
 
   el.addEventListener("submit", function(e) {
     e.preventDefault();
-    console.log(document.getElementById('email').value);
 
-    try {
-      var xhr = new XMLHttpRequest();
-      xhr.open("POST", action);
-      xhr.send(JSON.stringify({
-        email: document.getElementById('email').value,
-        from: document.getElementById('from').value
-      }));
-      message.innerHTML = "Thanks, you're signed up!"
-      form.remove();
-    } catch(e) {
-      message.innerHTML = "Sorry, there was an error. Please try again or email "
+    if (!honeypotField.value) {
+
+      let formData = new FormData(e.target);
+
+      try {
+        var xhr = new XMLHttpRequest();
+        xhr.open("POST", action);
+        xhr.send(JSON.stringify(
+          Object.fromEntries(formData)
+        ));
+        message.innerHTML = "Thanks, you're signed up!"
+        form.remove();
+        message.classList.remove('hidden');
+      } catch(e) {
+        message.innerHTML = `Sorry, there was an error. Please try again or email us at <a href="mailto:hello@madebykind.com">hello@madebykind.com</a>.`
+        message.classList.remove('hidden');
+      }
     }
 
   });
