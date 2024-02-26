ab3752caf013107b6143b73af3c8d80e4316fc73
Mat Hayward <mat@madebykind.com>
2024-02-26 15:00:20 +0000

    update manifest and clear console

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ import initModules from "./js/initModules";
 import modulesEnabled from "./js/modulesEnabled";
 
 // JS Goes here - ES6 supported
-console.log("Sup?! Edit me in src/main.js");
+// console.log("Sup?! Edit me in src/main.js");
 
 // Vue.config.productionTip = false;
 
