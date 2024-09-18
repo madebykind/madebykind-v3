28b8f3ea2e94fc92dd4b7c3a1618b293d1f1a824
Mat Hayward <mat@madebykind.com>
2024-09-18 18:46:43 +0100

    remove need for Zapier

--- src/js/subscribe-form.js
+++ src/js/subscribe-form.js
@@ -1,8 +1,8 @@
 function SubscribeForm(el) {
-  const action = el.getAttribute('action');
-  const form = el.querySelector("[data-form]");
-  const message = el.querySelector("[data-message]");
-  const successContent = message.dataset.messageSuccess;
+  // const action = el.getAttribute('action');
+  // const form = el.querySelector("[data-form]");
+  // const message = el.querySelector("[data-message]");
+  // const successContent = message.dataset.messageSuccess;
   const honeypotField = el.querySelector("input.h0n")
 
 
@@ -11,21 +11,23 @@ function SubscribeForm(el) {
 
     if (!honeypotField.value) {
 
-      let formData = new FormData(e.target);
+      el.submit();
 
-      try {
-        var xhr = new XMLHttpRequest();
-        xhr.open("POST", action);
-        xhr.send(JSON.stringify(
-          Object.fromEntries(formData)
-        ));
-        message.innerHTML = (successContent !== undefined) ? successContent : "Almost there... we've sent you an email to confirm your subscription."
-        form.remove();
-        message.classList.remove('hidden');
-      } catch(e) {
-        message.innerHTML = `Sorry, there was an error. Please try again or email us at <a href="mailto:hello@madebykind.com">hello@madebykind.com</a>.`
-        message.classList.remove('hidden');
-      }
+      // let formData = new FormData(e.target);
+
+      // try {
+      //   var xhr = new XMLHttpRequest();
+      //   xhr.open("POST", action);
+      //   xhr.send(JSON.stringify(
+      //     Object.fromEntries(formData)
+      //   ));
+      //   message.innerHTML = (successContent !== undefined) ? successContent : "Almost there... we've sent you an email to confirm your subscription."
+      //   form.remove();
+      //   message.classList.remove('hidden');
+      // } catch(e) {
+      //   message.innerHTML = `Sorry, there was an error. Please try again or email us at <a href="mailto:hello@madebykind.com">hello@madebykind.com</a>.`
+      //   message.classList.remove('hidden');
+      // }
     }
 
   });
