2fc203223f46abb5665ef79359c1c66bc0d8658a
Mat Hayward <mat@madebykind.com>
2025-04-03 17:40:31 +0100

    Lazy load newsletter image + remove unused JS

--- src/js/subscribe-form.js
+++ src/js/subscribe-form.js
@@ -1,33 +1,11 @@
 function SubscribeForm(el) {
-  // const action = el.getAttribute('action');
-  // const form = el.querySelector("[data-form]");
-  // const message = el.querySelector("[data-message]");
-  // const successContent = message.dataset.messageSuccess;
-  const honeypotField = el.querySelector("input.h0n")
-
+  const honeypotField = el.querySelector("input.h0n");
 
   el.addEventListener("submit", function(e) {
     e.preventDefault();
 
     if (!honeypotField.value) {
-
       el.submit();
-
-      // let formData = new FormData(e.target);
-
-      // try {
-      //   var xhr = new XMLHttpRequest();
-      //   xhr.open("POST", action);
-      //   xhr.send(JSON.stringify(
-      //     Object.fromEntries(formData)
-      //   ));
-      //   message.innerHTML = (successContent !== undefined) ? successContent : "Almost there... we've sent you an email to confirm your subscription."
-      //   form.remove();
-      //   message.classList.remove('hidden');
-      // } catch(e) {
-      //   message.innerHTML = `Sorry, there was an error. Please try again or email us at <a href="mailto:hello@madebykind.com">hello@madebykind.com</a>.`
-      //   message.classList.remove('hidden');
-      // }
     }
 
   });
