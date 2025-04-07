22aa11de0940c7f85269e1d77e914fa079c980ed
Mat Hayward <mat@madebykind.com>
2025-04-07 16:46:28 +0100

    home header and footer

--- content/_index.md
+++ content/_index.md
@@ -1,34 +1,36 @@
 ---
+title: Ethical creative agency for charities and non-profits
+date: 2025-04-07T10:48:46.000Z
+hidden: false
+
+
 cover_img: images/true-to-who-we-are-v1.png
 cover_img_alt: A giant friendly hand, holding a small flower
+preheading: Ethical creative agency
+heading: Lorem ipsum dolor sit
+intro: >
+
+
+  Phasellus egestas, erat ultricies sodales tempus, nisi justo fringilla nisi, [strategy](/services/strategy), [brand](/services/brand) and [digital](/services/digital) eleifend felis mi consectetur leo. Nam ut lorem urna.
+intro_md: true
+
+
+
+
 cta_img: images/raise-expectations-broccoli.png
 cta_img_alt: A giant friendly hand, watering a small plant with a tiny watering can.
-cta: More about kind
-ctaLink: /about/
-cta2: Our approach
-cta2Link: /services/#how-we-work
 cta3Title: Raise your expectations
 cta3Copy: Ethical work is excellent work. We combine expertise in digital, branding and strategy with an unwavering commitment to social change.
 cta3: Our work
 cta3Link: /work/
 cta4: Services
 cta4Link: /services/
-date: 2020-04-24T10:48:46.000Z
 description: We’re a small but powerful team of carefully selected senior professionals. Strategy, brand, digital – we bring it all together and drive change in ways that really matter.
-title: Strategy, Brand, Digital
+
 services:
   - brand
   - digital
   - strategy
-hidden: false
-heading: For the good folks
-intro: >
-
-  The **wildlife protectors**, **cultural contributors** and **green architects**. We’re for everyone who’s making a change for&nbsp;good.
-
-
-  That’s not you ~~BP~~, ~~Amazon~~, ~~Shell~~…
-intro_md: true
 related_stats:
   - pay-gap
   - socially-reponsible
--- tailwind.config.js
+++ tailwind.config.js
@@ -102,45 +102,33 @@ module.exports = {
       }
     },
     fontSize: {
-      /* @link https://utopia.fyi/type/calculator?c=375,18,1.333,1366,24,1.333,8,2,768-1024-1280&s=0.75|0.5|0.25,1.5|2|3|4|6,s-l&g=s,l,xl,12 */
-      
-      /* Step -2: 10.13px → 13.51px */
-      "xs": "clamp(0.6331rem, 0.5532rem + 0.3411vw, 0.8444rem)",
-      
-      /* Step -1: 13.50px → 18.00px */
-      "sm": "clamp(0.8438rem, 0.7373rem + 0.4541vw, 1.125rem)",
-      
-      /* Step 0: 18.00px → 24.00px */
-      "base": "clamp(1.125rem, 0.9831rem + 0.6054vw, 1.5rem)",
-      
-      /* Step 1: 23.99px → 31.99px */
-      "md": "clamp(1.4994rem, 1.3102rem + 0.8073vw, 1.9994rem)",
-      
-      /* Step 2: 31.98px → 42.65px */
-      "lg": "clamp(1.9988rem, 1.7464rem + 1.0767vw, 2.6656rem)",
-      
-      /* Step 3: 42.63px → 56.85px */
-      "xl": "clamp(2.6644rem, 2.3281rem + 1.4349vw, 3.5531rem)",
-      
-      /* Step 4: 56.83px → 75.78px */
-      "2xl": "clamp(3.5519rem, 3.1037rem + 1.9122vw, 4.7363rem)",
-      
-      /* Step 5: 75.76px → 101.01px */
-      "3xl": "clamp(4.735rem, 4.1378rem + 2.5479vw, 6.3131rem)",
-      
-      /* Step 6: 100.98px → 134.65px */
-      "4xl": "clamp(6.3113rem, 5.5149rem + 3.3976vw, 8.4156rem)",
-      
-      /* Step 7: 134.61px → 179.48px */
-      "5xl": "clamp(8.4131rem, 6.3519rem + 4.5277vw, 10.7175rem)",
-      
-      /* Step 8: 179.44px → 239.25px */
-      "6xl": "clamp(11.215rem, 9.8005rem + 6.0353vw, 14.9531rem)",
+      /* @link https://utopia.fyi/type/calculator?c=375,18,1.333,1860,24,1.333,8,2,1728&s=0.75|0.5|0.25,1.5|2|3|4|6,s-l&g=s,l,xl,12 */
+
+      /* Step -2: 10.1301px → 13.5068px */
+      "xs": "clamp(0.6331rem, 0.5798rem + 0.2274vw, 0.8442rem)",
+      /* Step -1: 13.5034px → 18.0045px */
+      "sm": "clamp(0.844rem, 0.7729rem + 0.3031vw, 1.1253rem)",
+      /* Step 0: 18px → 24px */
+      "base": "clamp(1.125rem, 1.0303rem + 0.404vw, 1.5rem)",
+      /* Step 1: 23.994px → 31.992px */
+      "md": "clamp(1.4996rem, 1.3734rem + 0.5386vw, 1.9995rem)",
+      /* Step 2: 31.984px → 42.6453px */
+      "lg": "clamp(1.999rem, 1.8307rem + 0.7179vw, 2.6653rem)",
+      /* Step 3: 42.6347px → 56.8462px */
+      "xl": "clamp(2.6647rem, 2.4404rem + 0.957vw, 3.5529rem)",
+      /* Step 4: 56.832px → 75.776px */
+      "2xl": "clamp(3.552rem, 3.253rem + 1.2757vw, 4.736rem)",
+      /* Step 5: 75.7571px → 101.0094px */
+      "3xl": "clamp(4.7348rem, 4.3363rem + 1.7005vw, 6.3131rem)",
+      /* Step 6: 100.9842px → 134.6456px */
+      "4xl": "clamp(6.3115rem, 5.7802rem + 2.2668vw, 8.4153rem)",
+      /* Step 7: 134.6119px → 179.4826px */
+      "5xl": "clamp(8.4132rem, 7.7051rem + 3.0216vw, 11.2177rem)",
+      /* Step 8: 179.4377px → 239.2503px */
+      "6xl": "clamp(11.2149rem, 10.2708rem + 4.0278vw, 14.9531rem)",
+
     },
   },
-  safelist: [
-    'xl:col-start-5',
-  ],
   variants: {
     extend: {
       fontSmoothing: ['dark'],
